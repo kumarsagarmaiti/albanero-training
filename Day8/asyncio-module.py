389e97d8b2d2b5fe910214fde1d530385fbe3c75
KingK9t8 <ksagarm789@gmail.com>
2023-01-30 11:20:37 +0530

    asyncio

--- Day8/asyncio-module.py
+++ Day8/asyncio-module.py
@@ -2,10 +2,11 @@ import asyncio
 
 
 async def print_name(fname: str, lname: str):
-    flag = True
+    # flag = True
     # to create an async behaviour we create tasks
     # task = asyncio.create_task(print_middle_name("Sagar"))
-    asyncio.ensure_future(print_middle_name("Sagar"))
+    # asyncio.ensure_future(print_middle_name("Sagar"))
+    task=asyncio.create_task(print_middle_name("Sagar"))
     print(fname)
     # can await an async function using the await keyword
     # mname = await (print_middle_name("Sagar"))
@@ -14,16 +15,20 @@ async def print_name(fname: str, lname: str):
     # print(mname)
     print(lname)
     # can await a task at the end so all the synchronous non-blocking code is run first
-    if flag:
-        return "hello"
+    # if flag:
+    #     return "hello"
     # await task
 
 
 async def print_middle_name(mname: str):
-    await asyncio.sleep(10)
+    await asyncio.sleep(5)
     print(mname)
 
 
-# asyncio.run(print_name("Kumar", "Maiti"))
-loop = asyncio.get_event_loop()
-loop.run_until_complete(print_name("Kumar", "Maiti"))
+asyncio.run(print_name("Kumar", "Maiti"))
+# loop = asyncio.get_event_loop()
+# loop.run_until_complete(print_name("Kumar", "Maiti"))
+
+import time
+time.sleep(10)
+print("done")
